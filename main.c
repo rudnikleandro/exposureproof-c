e8f8016ec2b70808050f9fe974fb5ee8880765bf
Leandro Rudnik <69438796+rudnikleandro@users.noreply.github.com>
2026-06-15 22:51:44 -0300

    add command line target argument

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("ExposureProof C - MVP inicial \n");
+int main(int argc, char *argv[]) {
+    if (argc !=2) {
+        printf("Usage: %s <domain>\n", argv[0]);
+        return 1;
+    }
+
+    printf("Target: %s\n", argv[1]);
+
     return 0;
+
 }
